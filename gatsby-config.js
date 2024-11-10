ff21547600d49e4091f4827e941379c2ca587a2a
Sijie-Yang <98970172+Sijie-Yang@users.noreply.github.com>
2024-11-10 13:31:25 +0800

    Setup Github Action

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
 module.exports = {
   siteMetadata: {
     title: `Thermal Affordance`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://ta.sijie-yang.com`
   },
   plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", "gatsby-transformer-remark", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
     // "gatsby-plugin-google-gtag" is not included because it requires a Google Analytics account
